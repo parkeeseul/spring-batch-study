4b4b91e8cb521850510a949390aee282436343f9
seul <eeseul.p@gmail.com>
2022-02-25 01:42:03 +0900

    <feat> spring batch 설정 추가

--- src/main/java/io/springbatch/springbatchstudy/SpringBatchStudyApplication.java
+++ src/main/java/io/springbatch/springbatchstudy/SpringBatchStudyApplication.java
@@ -1,13 +1,15 @@
 package io.springbatch.springbatchstudy;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchStudyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBatchStudyApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchStudyApplication.class, args);
+	}
 
 }
