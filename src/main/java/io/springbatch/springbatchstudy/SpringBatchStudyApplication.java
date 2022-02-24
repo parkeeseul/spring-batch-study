67728100c203e2f17c2eec6203b2d3f4f75fa946
seul <eeseul.p@gmail.com>
2022-02-25 01:41:15 +0900

    init

---
+++ src/main/java/io/springbatch/springbatchstudy/SpringBatchStudyApplication.java
@@ -0,0 +1,13 @@
+package io.springbatch.springbatchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/springbatch/springbatchstudy/SpringBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbatch.springbatchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
